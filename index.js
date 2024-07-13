38b534bccb8ae9ecc95c592e77a2f3bda13f18e0
+ <+>
2024-07-13 15:40:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,84 @@
 /* Your Code Here */
+const createEmployeeRecord = function (array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+]
+
+const createEmployeeRecords = function (array) {
+    let newArr = []
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(createEmployeeRecord(array[i]))
+  }
+  return newArr
+}  
+
+const createTimeInEvent = function (timeString) {
+    let timeInfo = timeString.split(' ')
+    let date = timeInfo[0]
+    let hour = parseInt(timeInfo[1], 10)
+
+    let timeEvent = {
+        type: 'TimeIn',
+        date: date,
+        hour: hour
+      }
+
+    this.timeInEvents.push(timeEvent)
+    return this
+}
+
+const createTimeOutEvent = function (timeString) {
+    let timeInfo = timeString.split(' ')
+    let date = timeInfo[0]
+    let hour = parseInt(timeInfo[1], 10)
+
+    let timeEvent = {
+        type: 'TimeOut',
+        date: date,
+        hour: hour
+      }
+
+    this.timeOutEvents.push(timeEvent)
+    return this
+}
+
+const  hoursWorkedOnDate = function (date) {
+    let inEvent = this.timeInEvents.find(item => item.date === date)
+    let outEvent = this.timeOutEvents.find(item => item.date === date)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+const wagesEarnedOnDate = function (date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+let src = [
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150]
+]
+
+const findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+console.log(findEmployeeByFirstName(src, 'Loki'))
+
+const calculatePayroll = function (employeeRecords) {
+    const allWagesOwed = employeeRecords.map(record => allWagesFor.call(record))
+    return allWagesOwed.reduce((acc, amount) => acc + amount)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
